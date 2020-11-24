a1d9f21663cbde28ddc344e5831af43ba29aad3c
zoubairGuesmi <zoubair.guesmi28@gmail.com>
2020-11-24 15:21:58 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EmployeeDetailsComponent} from './employee-details/employee-details.component';
+import {CreateEmployeeComponent} from './create-employee/create-employee.component';
+import {EmployeeListComponent} from './employee-list/employee-list.component';
+import {UpdateEmployeeComponent} from './update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'employees', pathMatch:'full'},
+  {path: 'employees', component: EmployeeListComponent},
+  {path: 'addEmployee', component: CreateEmployeeComponent},
+  {path: 'update/:id', component: UpdateEmployeeComponent},
+  {path: 'details/:id', component: EmployeeDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
